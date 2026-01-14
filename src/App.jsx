464c8b0aeb881f06c55265f2c88dd2ffc46b42ac
Ilesh <ilesh.25bai10018@vitbhopal.ac.in>
2026-01-14 22:05:20 +0530

    svg images for placing

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import NavBar from "./Components/NavBar/NavBar";
 import Tech from "./pages/Tech";
 import Home from "./pages/Home";
 import WebDTeam from "./pages/WebDTeam";
+import SplashScreen from "./pages/SplashScreen";
 
 function App() {
-    return (
-        <>
-            <NavBar />
-            <main>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/tech" element={<Tech />} />
-                    <Route path="/teams/tech/webDTeam" element={<WebDTeam />} />
-                    <Route path="*" element={<Navigate to="/" replace />} />
-                </Routes>
-            </main>
-        </>
-    );
+  const [showSplash, setShowSplash] = useState(true);
+
+  if (showSplash) {
+    return <SplashScreen onFinish={() => setShowSplash(false)} />;
+  }
+
+  return (
+    <>
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tech" element={<Tech />} />
+          <Route path="/teams/tech/webDTeam" element={<WebDTeam />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
